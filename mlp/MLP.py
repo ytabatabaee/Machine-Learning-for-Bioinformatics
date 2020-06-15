94267baa31109a645871e991882c019a1d249f1a
ce441-981-95104866 <yasamin.tabatabaee@gmail.com>
2020-06-15 14:12:22 +0430

    MLP drafts

--- mlp/MLP.py
+++ mlp/MLP.py
@@ -4,37 +4,52 @@ class MLP:
         hidden_size = 3
         output_size = 3
         self.hist = {'loss':[], 'acc':[]}
+        self.lr = 1e-2
         
-#         self.W1 = 
-#         self.b1 = 
-#         self.W2 = 
-#         self.b2 = 
+        self.W1 = torch.randn(input_size, hidden_size)
+        self.b1 = torch.randn(hidden_size)
+        self.W2 = torch.randn(hidden_size, output_size)
+        self.b2 = torch.randn(output_size)
 
     def softmax(self, x):
-        pass
+        e = torch.exp(x - torch.max(x))
+        return e / e.sum()
     
     def sigmoid(self, x):
-        pass
+        return 1 / (1 + torch.exp(x))
 
     def cross_entropy(self, y, o):
-        pass
+      return -torch.sum(y * torch.log(o + 1e-10)).item()
     
     def forward(self, x):
-#         h1 = sigmoid(x*W_1+b_1)
-#         o = softmax(h1*W_2 + b_2)
-#         return o
-        pass
+        self.h = self.sigmoid(torch.matmul(x, self.W1) + self.b1)
+        o = self.softmax(torch.matmul(self.h, self.W2) + self.b2)
+        return o
 
     def backward(self, y, o):
-        pass
+        dZ3 = o - y
+        dW2 = torch.matmul(self.h.T, dZ3)
+        db2 = torch.mean(dZ3, axis=0)
+        dh = torch.matmul(dZ3, self.W2.T)
+        dZ2 = dh * self.sigmoid(self.h) * (1 - self.sigmoid(self.h))
+        dW1 = torch.matmul(self.x.T, dZ2)
+        db1 = torch.mean(dZ2, axis=0)
+        
+        self.W1 -= self.lr * dW1
+        self.b1 -= self.lr * db1
+        self.W2 -= self.lr * dW2
+        self.b2 -= self.lr * db2
     
     def train(self, x, y, epochs):
+        self.x = x
         for epoch in tqdm(range(1, epochs+1)):
             o = self.forward(x)
             self.backward(y, o)
             
             loss = self.cross_entropy(y, o)
-            acc = accuracy_score(y, o)
+            acc = accuracy_score(np.argmax(y.numpy(), axis=1), np.argmax(o.numpy(), axis=1))
+            print(np.argmax(y.numpy(), axis=1))
+            print(np.argmax(o.numpy(), axis=1))
             self.hist['loss'] += [loss]
             self.hist['acc'] += [acc]
             print(epoch, 'loss:', loss, 'acc:', acc)
